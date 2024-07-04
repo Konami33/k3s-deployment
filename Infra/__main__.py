151e47c31b264735bc53cca1f30ae08b92b77f60
Konami33 <yasinarafat9889@gmail.com>
2024-07-05 01:32:40 +0600

    test7

--- Infra/__main__.py
+++ Infra/__main__.py
@@ -5,9 +5,9 @@ import pulumi_aws.ec2 as ec2
 from pulumi_aws.ec2 import SecurityGroupRuleArgs
 
 
-# Configuration
+# Configuration setup
 config = pulumi.Config()
-instance_type = 't3.small'
+instance_type = 't2.micro'
 ami = "ami-060e277c0d4cce553"
 
 
